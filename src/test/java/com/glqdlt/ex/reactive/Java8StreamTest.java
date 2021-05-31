658fc892768bb406b55b5201c528e5e6a1146f5d
jhun <jhun@zempot.com>
2021-05-31 19:01:22 +0900

    c

--- src/test/java/com/glqdlt/ex/reactive/Java8StreamTest.java
+++ src/test/java/com/glqdlt/ex/reactive/Java8StreamTest.java
@@ -3,9 +3,9 @@ package com.glqdlt.ex.reactive;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * @author jhun
@@ -20,6 +20,10 @@ public class Java8StreamTest {
      * 함수A에서 함수B로 흘러갈 때.. 함수A의 결과가 null 이라면 함수B는 호출이 되면 안된다. 이 경우는 함수B가 방어하면 넘어오는 인자가 null 을 체크하는 것으로 쉽게 생각할수 있지만,
      * 생각을 바꿔어보면 애초에 null일 때에 함수B가 호출이 안되게 되면 안될까? 란 생각을 해볼수 있다. 이러한 제어 흐름을 도와주는 것이 모나드이다.
      * Optional.empty() 정적 메소드를 통해 Empty 유형의 Optional 은 추후 함수들이 호출되지 않도록 다른 함수와의 연결을 생략시켜준다.
+     * 예를 들어서 Optional.empty 는 map(), filter() 와 같은 고차함수가 실행이 안되도록 한다.
+     * 즉 아래와 같은 흐름으로 진행이 가능하다.
+     * 정상케이스: fx() -> fx() -> fx() -> result
+     * empty일떄: fx() -> skip() -> skip() -> result
      * 이러한 함수 파이프의 연결 흐름에 문제가 생기거나 복잡도가 높아질수 있다.
      * 함수A의 결과 그 자체가 자기 자신이 함수B를 적용하는게 좋을 지 말지를 결정하게 하면 어떨까? 로 이해하면 모나드의 필요성을 이해할수가 있다.
      */
@@ -35,5 +39,48 @@ public class Java8StreamTest {
                 .filter(x -> x % 2 == 0)
                 .collect(Collectors.toList());
         Assert.assertEquals(TestDummyStub.EXAMPLE_1_STUB, result);
+    }
+
+    private final List<Integer> stub = IntStream.rangeClosed(1, 100)
+            .boxed()
+            .map(x -> x % 2 == 0 ? x : null)
+            .collect(Collectors.toList());
+
+    @Test
+    public void example2() {
+
+        stub.stream()
+                .map(Optional::ofNullable)
+                .map(x -> x.map(y -> y * 2))
+                .map(x -> x.orElse(0))
+                .forEach(x -> System.out.println(x));
+
+    }
+
+    @Test
+    public void example3() {
+        List<Integer> zxc = stub.stream()
+                .map(x -> x == null ? Arrays.asList(0, 0) : Collections.singletonList(x))
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+        Assert.assertEquals(150, zxc.size());
+    }
+
+    @Test
+    public void example4() {
+
+        Integer cc = Optional.empty()
+                .map(x -> (Integer) x)
+                .map(x -> x * 2)
+                .orElse(0);
+
+        Assert.assertTrue(cc.equals(0));
+
+        Optional.empty()
+                .map(x -> (Integer) x)
+                .flatMap(Optional::ofNullable)
+                .map(x -> x *  2)
+                .orElse(0);
+
     }
 }
